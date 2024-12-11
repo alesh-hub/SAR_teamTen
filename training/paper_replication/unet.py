22cf0e448187c33c54710376ae6f1740258b1d5c
alesh-hub <156431340+alesh-hub@users.noreply.github.com>
2024-12-11 01:34:04 +0100

    started to build the data module for paper replication

--- training/paper_replication/unet.py
+++ training/paper_replication/unet.py
@@ -1,1 +1,14 @@
 # TODO
+
+import numpy as np
+import os
+import pytorch_lightning
+import segmentation_models_pytorch as smp
+
+model = smp.Unet(
+    encoder_name="resnet101",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+    encoder_weights=None,     # use `imagenet` pre-trained weights for encoder initialization
+    in_channels=1,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+    classes=3,                      # model output channels (number of classes in your dataset)
+)
+
